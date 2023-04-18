56fa6f44b0a0065f111ed5ce66de3f5ad6327918
Marc Bertram <marc.bertram@gmail.com>
2023-04-18 18:12:41 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,12 +1,27 @@
-import openai
-openai.api_key = "API KEY" # https://platform.openai.com/account/api-keys
+#import openai
+apikey = "" # https://platform.openai.com/account/api-keys
+#openai.api_key = apikey 
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": "Wie kann man in Python eine Datei einlesen?"}
-  ]
-)
+#completion = openai.ChatCompletion.create(
+#  model="gpt-3.5-turbo",
+#  messages=[
+#    {"role": "user", "content": "Wie kann man in Python eine Datei einlesen?"}
+#  ]
+#)
 
-print(completion.choices[0].message)
+###
+# API Request with requests
 
+import requests # https://requests.readthedocs.io/en/latest/
+
+response = requests.post('https://api.openai.com/v1/chat/completions',
+                        headers={'Authorization': f'Bearer {apikey}'},
+                        json = {
+                                    "model": "gpt-3.5-turbo",
+                                    "messages": [{"role": "user", "content": "Hello!"}]
+                                }
+                        )
+
+completion = response.json()
+
+print(completion['choices'][0]['message'])
